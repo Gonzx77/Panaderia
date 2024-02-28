62e864e8f2274b3f576bda3229305fab95d7e13a
AndresGonzaloBeltranAlmeyda <160796511+AndresGonzaloBealtranAlmeyda@users.noreply.github.com>
2024-02-27 21:03:41 -0500

    Panaderia Update V2.0

--- Panaderia.py
+++ Panaderia.py
@@ -1,7 +1,7 @@
 panaderia = {
     'panesSalados': {"productos":list([
         {"nombre":"Baguette","valor":3000},
-        {"nombre: Pan de campo, valor":2500},
+        {"nombre":"Pan de campo", "valor":2500},
         {"nombre":"Pan de centeno","valor":3500},
         {"nombre":"Pan de ajo","valor":1000},
         {"nombre":"Pan de molde integral","valor":3000},
@@ -34,24 +34,54 @@ panaderia = {
         {"nombre":"Donas","valor":3000}])}
 }
 
-print(f"""{panaderia["panesSalados"]}
-      {panaderia["panesDulces"]}
-      {panaderia["postres"]}""")
-op1 = int(input("Ingresa la categoria a la que desea acceder: "))
-op2 = int(input("Ingresa el numero del producto a comprar: "))
+print(f"""
+0: Panes Salados
+1: Panes Dulces
+2: Postres""")
 
+op1 = int(input("\n Ingresa la categoria a la que desea acceder: "))
 
 if op1 == 0:
+    print("Haz seleccionado: Panes Salados")
+    for indice, producto in enumerate(panaderia["panesSalados"]["productos"]):
+        name = producto["nombre"]
+        value = producto["valor"]
+        print(f"""{indice} {name}, valor: {value}""")
     op1 = "panesSalados"
 elif op1 == 1:
+    print("Haz seleccionado: Panes Dulces")
+    for indice, producto in enumerate(panaderia["panesDulces"]["productos"]):
+        name = producto["nombre"]
+        value = producto["valor"]
+        print(f"""{indice} {name}, valor: {value}""")
     op1 = "panesDulces"    
 elif op1 == 2:
+    print("Haz seleccionado: Postres")
+    for indice, producto in enumerate(panaderia["postres"]["productos"]):
+        name = producto["nombre"]
+        value = producto["valor"]
+        print(f"""{indice} {name}, valor: {value}""")
     op1 = "postres"
 else:
     print("Este producto no existe")
 
 
-producto = panaderia[op1]["productos"][op2]
-nombre = producto["nombre"]
-valor = producto["valor"]
-print(f"""Haz seleccionado: {nombre}, valor: {valor}""")
+op2 = int(input("\n Ingresa el numero del producto a comprar: "))
+
+selectedProduct = panaderia[op1]["productos"][op2]
+selectedProductName = selectedProduct["nombre"]
+selectedProductValue = selectedProduct["valor"]
+
+compra = (f"""Haz seleccionado el producto {selectedProductName}, con un valor de: {selectedProductValue}""")
+print(compra)
+
+monto = selectedProduct["valor"]
+money = int(input("\n Ingrese su pago: "))
+
+if money >= monto:
+    cambio = money - monto
+    print(f"""Tu cambio es de: {cambio}""")
+
+else:
+    falta = monto - money
+    print(f"""El dinero no es suficiente, te hace falta: {falta}""")
